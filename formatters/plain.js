9c7db33ad6c0dc0b699437033313979d2d58da42
y.nevinskaya <y.nevinskaya@tbank.ru>
2024-12-23 12:32:54 +0500

    Вывод в JSON

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,25 +1,26 @@
 const formatPlain = (diff, parent = '') => {
   const lines = Object.entries(diff).flatMap(([key, value]) => {
-    const cleanKey = key.replace(/^(\+ |\-)/, '');// Убираем префиксы + или -
+    // Убираем префиксы '+' и '-' из ключей
+    const cleanKey = key.replace(/^(\+ |- )/, '').trim();
     const property = parent ? `${parent}.${cleanKey}` : cleanKey;
 
     if (typeof value === 'object' && !Array.isArray(value) && value !== null && !(key.startsWith('+') || key.startsWith('-'))) {
       return formatPlain(value, property);
     }
 
-    // Обработка обновлённых значений (ключ одновременно присутствует с префиксами - и +)
+    // Если ключ изменён: есть '- key' и '+ key'
     if (key.startsWith('- ') && `+ ${cleanKey}` in diff) {
       const newKey = `+ ${cleanKey}`;
       return `Property '${property}' was updated. From ${JSON.stringify(value)} to ${JSON.stringify(diff[newKey])}`;
     }
 
-    // Обработка удалённых значений
+    // Если ключ удалён
     if (key.startsWith('- ')) {
       return `Property '${property}' was removed`;
     }
 
-    // Обработка добавленных значений
-    if (key.startsWith('+ ') && !(`- ${cleanKey}` in diff)) {
+    // Если ключ добавлен
+    if (key.startsWith('+ ')) {
       return `Property '${property}' was added with value: ${JSON.stringify(value)}`;
     }
 
